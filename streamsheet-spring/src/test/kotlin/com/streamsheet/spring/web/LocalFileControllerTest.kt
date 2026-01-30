40b7228806794595c6c7e06fa172404b3ff0acf4
danpung2 <danpung_2@naver.com>
2026-01-30 11:43:17 +0900

    refactor(spring): deprecated된 MockBean을 MockitoBean으로 교체

--- streamsheet-spring/src/test/kotlin/com/streamsheet/spring/web/LocalFileControllerTest.kt
+++ streamsheet-spring/src/test/kotlin/com/streamsheet/spring/web/LocalFileControllerTest.kt
@@ -4,23 +4,24 @@ import com.streamsheet.spring.autoconfigure.StreamSheetProperties
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import java.nio.file.Files
-import java.util.*
 
 @WebMvcTest(LocalFileController::class)
+@AutoConfigureMockMvc(addFilters = false)
 @DisplayName("LocalFileController 테스트")
 class LocalFileControllerTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @MockBean
+    @MockitoBean
     private lateinit var properties: StreamSheetProperties
 
     @Test
